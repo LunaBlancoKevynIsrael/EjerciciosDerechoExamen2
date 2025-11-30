eee21c552f1d112f9aaf2a4118bc469705edfbf0
LunaBlancoKevynIsrael <zS24015615@estudiantes.uv.mx>
2025-11-29 19:03:21 -0600

    Ejercicio07_TablaMultiplicar.java

--- Ejercicio07_TablaMultiplicar.java
+++ Ejercicio07_TablaMultiplicar.java
@@ -8,6 +8,5 @@ public class Ejercicio07_TablaMultiplicar {
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + " x " + i + " = " + (n * i));
         }
-        sc.close();
     }
 }
