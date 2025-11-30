11fb64db1fafe24cbb76487bdc5bc536f9fd54b0
LunaBlancoKevynIsrael <zS24015615@estudiantes.uv.mx>
2025-11-29 19:01:45 -0600

    Ejercicio08_AhorroInteres.java

--- Ejercicio08_AhorroInteres.java
+++ Ejercicio08_AhorroInteres.java
@@ -1,7 +1,7 @@
 public class Ejercicio08_AhorroInteres {
     public static void main(String[] args) {
         double ahorroMensual = 500.0;
-        double interesMensual = 0.09; // 9% cada mes
+        double interesMensual = 0.09;
         double saldo = 0.0;
         System.out.println("Mes\tSaldo al final del mes");
         for (int mes = 1; mes <= 12; mes++) {
