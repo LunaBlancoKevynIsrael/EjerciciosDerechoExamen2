ddf2a099700896908b905684f467a00de587160a
LunaBlancoKevynIsrael <zS24015615@estudiantes.uv.mx>
2025-11-29 19:02:03 -0600

    Ejercicio01_NombresWhile.java

--- Ejercicio01_NombresWhile.java
+++ Ejercicio01_NombresWhile.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class Ejercicio01_NombresWhile {
     public static void main(String[] args) {
-        // Muestra 10 nombres usando while
         String[] nombres = {"Ana Perez","Luis Gomez","María López","Jorge Ruiz","Carla Díaz","Pedro Sánchez","Lucía Morales","David Hernández","Patricia Torres","Marco Alan"};
         int i = 0;
         while (i < nombres.length) {
